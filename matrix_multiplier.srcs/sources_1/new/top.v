d2a63cbc6e7c1e7dfb79fd9fbf74128a7e57dbe0
ShriP <spate8@ocdsb.ca>
2026-08-03 11:18:57 -0400

    added a top module and connected the matrix output reg ports to wires

--- matrix_multiplier.srcs/sources_1/new/top.v
+++ matrix_multiplier.srcs/sources_1/new/top.v
@@ -21,6 +21,34 @@
 
 
 module top(
-
+ 
     );
+    //wires used to connect the ports using matrix 1
+    wire [3:0] a11_1_join;
+    wire [3:0] a12_1_join;
+    wire [3:0] a21_1_join;
+    wire [3:0] a22_1_join;
+    //wires used to connect the ports using matrix 2
+    wire [3:0] a11_2_join;
+    wire [3:0] a12_2_join;
+    wire [3:0] a21_2_join;
+    wire [3:0] a22_2_join;
+    //wires used to connect the ports using matrix 3 
+    wire [7:0] a11_3_join;
+    wire [7:0] a12_3_join;
+    wire [7:0] a21_3_join;
+    wire [7:0] a22_3_join;
+    
+    input_numbers u_input(
+        //connecting matrix 1 ports 
+        .a11_1(a11_1_join),
+        .a12_1(a12_1_join),
+        .a21_1(a21_1_join),
+        .a22_1(a22_1_join),
+        //connecting matrix 2 ports 
+        .a11_1(a11_2_join),
+        .a12_1(a12_2_join),
+        .a21_1(a21_2_join),
+        .a22_1(a22_2_join)
+        );
 endmodule
