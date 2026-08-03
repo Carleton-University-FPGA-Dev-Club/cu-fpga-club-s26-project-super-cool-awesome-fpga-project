5d006672617d488fa345801b21ef2264a0afb3d2
ShriP <spate8@ocdsb.ca>
2026-08-03 12:58:42 -0400

    instatiate module input_numbers --> completion not verified yet

--- matrix_multiplier.srcs/sources_1/new/top.v
+++ matrix_multiplier.srcs/sources_1/new/top.v
@@ -21,7 +21,13 @@
 
 
 module top(
- 
+    //input and output variables that will be used to instatiate the I/O variables from module input_numbers
+    input wire clock,
+    input wire enter_button,
+    input wire reset_button, 
+    input wire [3:0] number_switch,
+    output wire [3:0] counter,
+    output wire [3:0] led_out
     );
     //wires used to connect the ports using matrix 1
     wire [3:0] a11_1_join;
@@ -39,6 +45,7 @@ module top(
     wire [7:0] a21_3_join;
     wire [7:0] a22_3_join;
     
+    //instatiate the module input_numbers 
     input_numbers u_input(
         //connecting matrix 1 ports 
         .a11_1(a11_1_join),
@@ -49,6 +56,14 @@ module top(
         .a11_1(a11_2_join),
         .a12_1(a12_2_join),
         .a21_1(a21_2_join),
-        .a22_1(a22_2_join)
+        .a22_1(a22_2_join),
+        //connecting the input wires to the input variables in module input_numbers
+        .System_clock(clock),
+        .enter(enter_button),
+        .reset(reset_button),
+        .number(numbmer_switch),
+        //connecting the wires to the output variables in module input_numbers
+        .index(counter),
+        .led(led_out)
         );
 endmodule
